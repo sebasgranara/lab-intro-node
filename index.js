06e17dace9539cf805131e0fcb02ea6d7761a1ac
Sebas Granara <sebastian.granara@gmail.com>
2021-11-22 20:00:45 +0100

    hasta interaccion 5

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let pos = 0;
 
-  get(pos) {}
+    while (this.items[pos] < item){
+      pos++;
+    }
+    this.items.splice(pos, 0, item)
+    
+  }
 
-  max() {}
+  get(pos) {
+    if (pos >= this.length){
+      throw new Error("OutOfBounds")
+    }
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.length == 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length -1];
+  }
+
+  min() {
+    if (this.length == 0){
+      throw new Error('EmptySortedList');
+
+  }
+  return this.items[0]
+}
 
   sum() {}
 
